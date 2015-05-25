cdf996a0877e9ed377de2c5796d879cb0fe30e64
Brecht Gosselé <brecht.gossele@gmail.com>
2015-05-25 11:01:50 +0200

    benchmarking queries + diploïdie

---
+++ gemini_src/benchmarking_queries.sh
@@ -0,0 +1,25 @@
+	gemini query -q "select chrom,start,end,pi from variants \
+               	where  = 'ts' \
+        	and call_rate >= 0.95" 
+	
+	gemini query -q "select chrom, start, end, ref, alt, gene from variants" \
+	               --gt-filter "gt_types.HG00239 == HET || gt_types.NA19377 == HOM_REF" 
+
+	gemini query -q "select chrom, start, end, ref, alt, gene from variants" \
+	               --gt-filter "gt_types.HG00239 == HET && gt_types.NA19377 == HOM_REF" 
+
+	gemini query -q "select chrom, start, end, ref, alt, gene from variants" \
+	               --gt-filter "gt_types.HG00239 != HET" 
+
+	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
+	               --gt-filter "[gt_types].[phenotype=='2'].[!=HOM_REF].[all]" 
+	
+	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
+	               --gt-filter "[gt_types].[phenotype=='2'].[==HOM_REF].[all] && \
+	                            	[gt_depths].[phenotype=='2'].[>=20].[all]" 
+
+	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
+	               --gt-filter "[gt_depths].[phenotype=='2'].[<10].[count <= 20] && \
+	               			[gt_types].[phenotype=='2'].[!=HOM_REF].[all]" 
+
+
