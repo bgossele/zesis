c974a4ed86d9c5137542ef54e8923847b83d6123
Brecht Gosselé <brecht.gossele@gmail.com>
2015-05-26 22:40:35 +0200

    resultaten query benchmark ifv GEMINI threads

--- gemini_src/benchmarking_queries.sh
+++ gemini_src/benchmarking_queries.sh
@@ -1,14 +1,14 @@
-	gemini query -q "select chrom,start,end,pi from variants \
-               	where  = 'ts' \
+	gemini query -q "SELECT chrom,start,end,pi FROM variants \
+               	WHERE  = 'ts' \
         	and call_rate >= 0.95" 
 	
-	gemini query -q "select chrom, start, end, ref, alt, gene from variants" \
+	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
 	               --gt-filter "gt_types.HG00239 == HET || gt_types.NA19377 == HOM_REF" 
 
-	gemini query -q "select chrom, start, end, ref, alt, gene from variants" \
+	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
 	               --gt-filter "gt_types.HG00239 == HET && gt_types.NA19377 == HOM_REF" 
 
-	gemini query -q "select chrom, start, end, ref, alt, gene from variants" \
+	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
 	               --gt-filter "gt_types.HG00239 != HET" 
 
 	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
@@ -22,4 +22,6 @@
 	               --gt-filter "[gt_depths].[phenotype=='2'].[<10].[count <= 20] && \
 	               			[gt_types].[phenotype=='2'].[!= HOM_REF].[all]" 
 
-
+	gemini query -q "SELECT chrom,start FROM variants \
+                         WHERE chrom = 'chr22' and start > 16000000 \
+                        	and start <= 18000000"
