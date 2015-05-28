60a3a82baab5e68c8bcf593bff4f72c429ad1f62
Brecht Gosselé <brecht.gossele@gmail.com>
2015-05-28 15:25:53 +0200

    gemini load benchmark resultaten

--- gemini_src/benchmarking_queries.sh
+++ gemini_src/benchmarking_queries.sh
@@ -1,26 +1,26 @@
 	gemini query -q "SELECT chrom,start,end,pi FROM variants \
-               	WHERE  = 'ts' \
-        	and call_rate >= 0.95" 
+        	       	 WHERE  = 'ts' \
+       				and call_rate >= 0.95" 
 	
 	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
-	               --gt-filter "gt_types.HG00239 == HET || gt_types.NA19377 == HOM_REF" 
+		--gt-filter "gt_types.HG00239 == HET || gt_types.NA19377 == HOM_REF" 
 
 	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
-	               --gt-filter "gt_types.HG00239 == HET && gt_types.NA19377 == HOM_REF" 
+	        --gt-filter "gt_types.HG00239 == HET && gt_types.NA19377 == HOM_REF" 
 
 	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
-	               --gt-filter "gt_types.HG00239 != HET" 
+	        --gt-filter "gt_types.HG00239 != HET" 
 
 	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
-	               --gt-filter "[gt_types].[phenotype=='2'].[!= HOM_REF].[all]" 
+	        --gt-filter "[gt_types].[phenotype=='2'].[!= HOM_REF].[all]" 
 	
 	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
-	               --gt-filter "[gt_types].[phenotype=='2'].[== HOM_REF].[all] && \
-	                            	[gt_depths].[phenotype=='2'].[>=20].[all]" 
+	        --gt-filter "[gt_types].[phenotype=='2'].[== HOM_REF].[all] && \
+                            	[gt_depths].[phenotype=='2'].[>=20].[all]" 
 
 	gemini query -q "SELECT chrom, start, end, ref, alt, gene FROM variants" \
-	               --gt-filter "[gt_depths].[phenotype=='2'].[<10].[count <= 20] && \
-	               			[gt_types].[phenotype=='2'].[!= HOM_REF].[all]" 
+	        --gt-filter "[gt_depths].[phenotype=='2'].[<10].[count <= 20] && \
+               			[gt_types].[phenotype=='2'].[!= HOM_REF].[all]" 
 
 	gemini query -q "SELECT chrom,start FROM variants \
                          WHERE chrom = 'chr22' and start > 16000000 \
